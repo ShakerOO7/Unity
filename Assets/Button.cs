47a513c9f9c160b776062d2a0fbc26db059f8b73
Shaker <shakersyr@gmail.com>
2020-03-06 19:26:03 +0200

    jgd
    526

---
+++ Assets/Button.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    GameObject[] mShip;
+    public int shipCount = 20, last = 0;
+    public Rigidbody Ship;
+    public GameObject Source, Target;
+    public float Speed = 5.0f;
+    bool Click = false;
+
+    void Start()
+    {
+        mShip = new GameObject[12000];
+    }
+
+    public void OnClick()
+    {
+        Click = true;
+        last = last % 10000;
+        for(int i = 0; i  < shipCount; i++,last++)
+        {
+            //Debug.Log(last);
+            GetShip(last);
+        }
+    }
+
+    void Update()
+    {
+        if (Click)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                if (mShip[i] != null)
+                {
+                    mShip[i].GetComponent<Rigidbody>().velocity = (Target.transform.position
+                            - mShip[i].transform.position).normalized * Speed;
+                }
+            }
+                
+        }
+    }
+
+    void GetShip(int i)
+    {
+        mShip[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        mShip[i].GetComponent<Renderer>().material = Ship.GetComponent<Renderer>().material;
+        mShip[i].transform.localScale = Ship.transform.localScale;
+        mShip[i].transform.position = Source.transform.position;
+        mShip[i].AddComponent<Collider>();
+        mShip[i].AddComponent<Rigidbody>();
+        mShip[i].GetComponent<Rigidbody>().useGravity = false;
+        mShip[i].GetComponent<Rigidbody>().velocity = (Target.transform.position
+            - mShip[i].transform.position).normalized * Speed;
+    }
+}
---
+++ Assets/blocks.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blocks : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public GameObject cube, block;
+	public Material mat;
+    float time;
+    void Start()
+    {
+        //Create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //time += Time.deltaTime;
+        //if (time > 1)
+        //{
+        //    Create();
+        //}
+    }
+
+    public void Create()
+    {
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = block.transform.position;
+        cube.AddComponent<Rigidbody>();
+        cube.GetComponent<Rigidbody>().useGravity = true;
+        time = Time.deltaTime;
+        cube.GetComponent<Renderer>().material = mat;
+        switch (Random.Range(1, 3))
+        {
+            case 1:
+                cube.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                break;
+            case 2:
+                cube.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+                break;
+        }
+        
+    }
+}
